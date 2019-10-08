8df83092bb0c4610f98878bbe68677e4d56bc662
Max98002 <maksimka98002@gmail.com>
2019-10-08 10:23:22 +0300

    Apdate arrow

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,9 +16,34 @@ $(document).ready(function () {
   }
   setInterval(move, 5000);
 
-  $('#up').click(function () {
-    $('body,html').animate({ scrollTop: 0 }, 500);
-    return false;
-  })
+  // $('#up').click(function () {
+  //   $('body,html').animate({ scrollTop: 0 }, 500);
+  //   return false;
+  // })
 
 });
+
+
+$(function() {
+ 
+  $(window).scroll(function() {
+   
+  if($(this).scrollTop() != 0) {
+   
+  $('#up').fadeIn();
+   
+  } else {
+   
+  $('#up').fadeOut();
+   
+  }
+   
+  });
+   
+  $('#up').click(function() {
+   
+  $('body,html').animate({scrollTop:0},800);
+   
+  });
+   
+  });
